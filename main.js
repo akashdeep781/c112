0f3b8ee38cc85b497b99ecc4efa482f2d0d6772c
Pihu sharma <104310896+akashdeep781@users.noreply.github.com>
2022-10-17 19:23:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:"png",
+    png_quality:90
+});
+camera=document.getElementById("camera");
+
+Webcam.attach(camera);
+
+function take_snapshot() {
+   Webcam.snap(function(data_uri){
+    document.getElementById("result").innerHTML="<img id='capture_image' src="+data_uri+">";
+   });
+}
+
+console.log("ml5 version:",ml5.version);
+
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/OYuU4i2oN/model.json",modelLoaded);
+
+function modelLoaded()
+{
+    console.log("modelLoaded");
+}
+
+function check() {
+    img=document.getElementById("capture_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, result) {
+if(error){
+    console.error(error);
+    }else{
+        console.log(result);
+        document.getElementById("result_label").innerHTML=result[0].label;
+        document.getElementById("result_confidence").innerHTML=result[0].confidence;
+    }}
